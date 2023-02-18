b5bfbc5857eef6a08c4b8dca108aad999533f16e
hayashiyoshino <twinklestaryoshino@gmail.com>
2023-02-18 22:27:02 +0900

    Initial Commit

---
+++ MyTimer/MyTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTimerApp.swift
+//  MyTimer
+//
+//  Created by Yoshino Hayashi on 2023/02/18.
+//
+
+import SwiftUI
+
+@main
+struct MyTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
