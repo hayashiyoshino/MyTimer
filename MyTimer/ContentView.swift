b43183304ae3a9780b0b5c8b02b17ac8d44a06a4
hayashiyoshino <twinklestaryoshino@gmail.com>
2023-02-19 09:01:53 +0900

    feature: 画面遷移できるよう設定

--- MyTimer/ContentView.swift
+++ MyTimer/ContentView.swift
@@ -9,13 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        // 階層的な画面遷移を管理する機能
+        NavigationStack {
+            VStack {
+                Text("タイマー画面")
+            }
+            // ナビゲーションにボタン追加
+            .toolbar {
+                // ナビゲーションバーの右にボタンを追加
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    // ナビゲーション繊維
+                    NavigationLink {
+                        SettingView()
+                    } label: {
+                        // テキストを表示
+                        Text("秒数設定")
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
