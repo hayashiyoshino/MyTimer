e8d3ad33396376a0facfca15c6ba62c9d4c60acd
hayashiyoshino <twinklestaryoshino@gmail.com>
2023-02-22 22:40:07 +0900

    feature: Pickerを配置

--- MyTimer/SettingView.swift
+++ MyTimer/SettingView.swift
@@ -18,6 +18,28 @@ struct SettingView: View {
             Color("backgroundSetting")
             // セーフエリアを超えて画面全体に配置
                 .ignoresSafeArea()
+            // 縦方向にレイアウト
+            VStack {
+                // Pickerを表示
+                Picker(selection: $timerValue) {
+                    Text("10")
+                        .tag(10)
+                    Text("20")
+                        .tag(20)
+                    Text("30")
+                        .tag(30)
+                    Text("40")
+                        .tag(40)
+                    Text("50")
+                        .tag(50)
+                    Text("60")
+                        .tag(60)
+                } label: {
+                    Text("選択")
+                }
+                // Pickerをホイール表示
+                .pickerStyle(.wheel)
+            }
         }
     }
 }
